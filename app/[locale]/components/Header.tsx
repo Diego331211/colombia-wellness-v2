aeec0b1b2cc79c30598c08f3a0719af1a58d0233
Botopia-SAS <botopia.sas@gmail.com>
2025-10-10 13:39:42 -0500

    CAmbios del correo

--- app/[locale]/components/Header.tsx
+++ app/[locale]/components/Header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
         </a>
 
         {/* Ícono de correo con enlace */}
-        <a href="contacto@colombiawellnessweek.co" title="Enviar correo">
+        <a href="mailto:contacto@colombiawellnessweek.co" title="Enviar correo">
           <FontAwesomeIcon
             icon={faEnvelope}
             className="h-7 hover:text-orange-500 transition-colors"
